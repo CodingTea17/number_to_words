d1e9e12c358a73e5949dbd04e2263b4faf695646
Dawson Mortenson and Lainey Rokkan <dawson@>
2017-09-05 16:45:53 -0700

    add code to split the input into individual numbers and add key value pairs

--- lib/number_words.rb
+++ lib/number_words.rb
@@ -1,18 +1,23 @@
 class Integer
   def number_words
-    numbers = Hash[0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four"]
 
+    # Partial list of required numbers
+    ones = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 6 => "six", 7 => "seven"}
+    tens = {2 => "twenty ", 7 => "seventy ", 8 => "eighty "}
+
+    # List of denomination names
+    denoms = {1 => " hundred ", 2 => " thousand ", 3 => " million ", 4 => " billion ", 5 => " trillion "}
+
+    # Splits a number into an array of string numbers (ex. 1234 -> "1","2","3","4")
     input_nums = self.to_s.split("")
-
+    # Empty array
     num_array = []
-
+    # Loops through array of presplit input numbers to reconvert to integer
     for n in input_nums
       num_array.push(n.to_i)
     end
-
-    num_of_places = num_array.length / 3
-    if num_of_places <= 1
-      return numbers[num_array[0]] + " hundred " + numbers[tens[num_array[1] + 4]] + numbers[num_array[2]]
+    
+    num_of_places = (num_array.length / 3).ceil
 
   end
 end
--- spec/number_words_spec.rb
+++ spec/number_words_spec.rb
@@ -5,4 +5,7 @@ describe("Integer#number_words") do
   it("will print a number up to 999 as words") do
     expect(384.number_words).to(eq("three hundred eighty four"))
   end
+  it("will print a number up to 999999 as words") do
+    expect(784376.number_words).to(eq("seven hundred eighty four thousand three hundred seventy six"))
+  end
 end
